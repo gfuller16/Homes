e72812e72f755ea704485161d2ce97b4b2f75555
gfuller16 <graysonfuller16@gmail.com>
2019-05-07 15:58:35 -0400

    Create tblFeel.sql

---
+++ tblFeel.sql
@@ -0,0 +1,21 @@
+USE [HomesDB]
+GO
+
+/****** Object:  Table [dbo].[tblFeel]    Script Date: 5/7/2019 3:58:20 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblFeel](
+	[fID] [int] NOT NULL,
+	[fDescription] [varchar](25) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[fID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
